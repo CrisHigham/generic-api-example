e0d99065fa573aaf024830bc521db5046193684d
CrisHigham <cris@justblink.co.za>
2025-04-08 12:13:52 +0200

    First release
    Added exclude passwords
    Added postman collection
    upgraded composer

--- src/orm/TestUser.php
+++ src/orm/TestUser.php
@@ -4,6 +4,7 @@ class TestUser extends \Tina4\ORM
     public $tableName="test_user";
     public $primaryKey="id"; //set for primary key
     public $genPrimaryKey=true; //set to true if you want to set the primary key
+    public $excludeFields = ["password"];
 
     public $id;
     public $firstName;
