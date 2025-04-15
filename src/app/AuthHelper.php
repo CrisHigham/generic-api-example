37d2d2d7262797e426740beea3785e66167e6a04
CrisHigham <cris@justblink.co.za>
2025-04-15 14:25:38 +0200

    Added bruno as test
    Removed postman
    Small auth change to avoid issues with no env set.
    committed database to avoid the need to run migrations

--- src/app/AuthHelper.php
+++ src/app/AuthHelper.php
@@ -4,7 +4,7 @@ class AuthHelper extends \Tina4\Auth
 {
     function validToken(string $token, string $publicKey = "", string $encryption = \Nowakowskir\JWT\JWT::ALGORITHM_RS256): bool
     {
-        if ($token === "Bearer " . $_ENV["TOKEN"]) {
+        if (isset($_ENV["TOKEN"]) && ($token === "Bearer " . $_ENV["TOKEN"])) {
             return true;
         }
         return parent::validToken($token, $publicKey, $encryption);
