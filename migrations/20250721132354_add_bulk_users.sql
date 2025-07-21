c6a553f39ac4df1e73bbc8b9730034bfd31f20bc
CrisHigham <cris@justblink.co.za>
2025-07-21 17:29:08 +0200

    Updated bruno
    Added more tests for the limit and offset

---
+++ migrations/20250721132354_add_bulk_users.sql
@@ -0,0 +1,59 @@
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (1, 'Clark', 'Kent', 'clark.kent@dailyplanet.com', 'superman123', 5551234567, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (2, 'Bruce', 'Wayne', 'bruce.wayne@wayneenterprises.com', 'batman123', 5559876543, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (3, 'Diana', 'Prince', 'diana.prince@themiscira.gov', 'wonderwoman123', 5552345678, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (4, 'Peter', 'Parker', 'peter.parker@dailybugle.com', 'spiderman123', 5553456789, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (5, 'Tony', 'Stark', 'tony.stark@starkindustries.com', 'ironman123', 5554567890, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (6, 'Steve', 'Rogers', 'steve.rogers@avengers.com', 'capamerica123', 5555678901, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (7, 'Natasha', 'Romanoff', 'natasha.romanoff@shield.com', 'blackwidow123', 5556789012, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (8, 'Bruce', 'Banner', 'bruce.banner@avengers.com', 'hulk123', 5557890123, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (9, 'Barry', 'Allen', 'barry.allen@ccpd.gov', 'flash123', 5558901234, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (10, 'Arthur', 'Curry', 'arthur.curry@atlantis.gov', 'aquaman123', 5559012345, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (11, 'Hal', 'Jordan', 'hal.jordan@greenlanterncorp.com', 'greenlantern123', 5551122334, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (12, 'Scott', 'Lang', 'scott.lang@pymtech.com', 'antman123', 5552233445, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (13, 'TChalla', '', 'tchalla@wakanda.gov', 'blackpanther123', 5553344556, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (14, 'Stephen', 'Strange', 'stephen.strange@sanctumsanctorum.com', 'drstrange123', 5554455667, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (15, 'Wanda', 'Maximoff', 'wanda.maximoff@avengers.com', 'scarletwitch123', 5555566778, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (16, 'Victor', 'Stone', 'victor.stone@starlabs.com', 'cyborg123', 5556677889, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (17, 'Logan', '', 'logan@xmen.org', 'wolverine123', 5557788990, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (18, 'Jean', 'Grey', 'jean.grey@xmen.org', 'phoenix123', 5558899001, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (19, 'Ororo', 'Munroe', 'ororo.munroe@xmen.org', 'storm123', 5559900112, 1);
+
+INSERT INTO test_user (id, first_name, last_name, email, password, cellphone, is_active)
+VALUES (20, 'Charles', 'Xavier', 'charles.xavier@xmen.org', 'professorx123', 5551011121, 1);
