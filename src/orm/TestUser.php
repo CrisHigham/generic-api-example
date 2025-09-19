46832b5b5c479809217e7e4d2fd32205fafc43d2
CrisHigham <cris@justblink.co.za>
2025-09-19 08:56:36 +0200

    Updated tests to match version 0.0.5

--- src/orm/TestUser.php
+++ src/orm/TestUser.php
@@ -5,6 +5,7 @@ class TestUser extends \Tina4\ORM
     public $primaryKey="id"; //set for primary key
     public $genPrimaryKey=true; //set to true if you want to set the primary key
     public $excludeFields = ["password"];
+    public $fieldMapping=["firstName" => "first_name"];
 
     public $id;
     public $firstName;
